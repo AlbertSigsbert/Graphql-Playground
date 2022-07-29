c8eedac1f820fe887153342352e1d75a0f5e3daf
Albert <ishengomaalbert@gmail.com>
2022-07-29 11:31:20 +0300

    Created a BookList component

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BookList(props) {
+    return (
+        <div id='book-list'>
+            <ul>
+                <li>Book One</li>
+            </ul>
+        </div>
+    );
+}
+
+export default BookList;
